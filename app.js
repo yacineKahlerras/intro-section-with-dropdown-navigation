cf8fe6dad879c6fd5dfb9d5493f8a5f9deadcade
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-08-17 00:58:30 +0100

    sidemenu

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /** vars */
 const btns = [...document.getElementsByTagName("button")];
 const sidebarContainer = document.querySelector(".sidebar-container");
+const sidebarSubmenus = [...document.querySelectorAll(".sidebar-submenu")];
 
 /** handles all button clicks */
 const btnHandler = (btn) => {
@@ -9,6 +10,13 @@ const btnHandler = (btn) => {
     btn.classList.contains("close-button")
   ) {
     sidebarContainer.classList.toggle("show-sidebar");
+    sidebarSubmenus.forEach((s) => s.classList.remove("show-sidebar-submenu"));
+  }
+  if (btn.classList.contains("features-sidebar")) {
+    sidebarSubmenus[0].classList.toggle("show-sidebar-submenu");
+  }
+  if (btn.classList.contains("company-sidebar")) {
+    sidebarSubmenus[1].classList.toggle("show-sidebar-submenu");
   }
 };
 
